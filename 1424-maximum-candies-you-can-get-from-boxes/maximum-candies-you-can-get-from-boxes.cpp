9a99e5057a694270b12e5d397716a8fb233bd2a8
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-06-03 23:36:12 +0530

    Time: 7 ms (44.71%) | Memory: 43.9 MB (91.35%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-
-        int MAX = 0 ;
-        int n = status.size() ;
-        vector<bool> visited(n,false) , hasBox(n,false) ;
-        queue<int> q ;
-
-        for ( int i : initialBoxes ){
-            hasBox[i] = true ;
-            if ( status[i] == 1 && !visited[i] ) q.push(i) ;
+        int n=status.size();
+        vector<bool> AvailableBoxes(n,false), AvailableKeys(n,false), visited(n,false);
+        queue<int> q;
+        for(int box:initialBoxes){
+            if(status[box]==1){
+                q.push(box);
+                visited[box]=true;
+            }
+            AvailableBoxes[box]=true;
         }
-
-        while ( !q.empty() ){
-
-            auto index = q.front() ;
-            q.pop() ;
-            if ( visited[index] ) continue ;
-            visited[index] = true ;
-            MAX += candies[index] ;
-
-            for ( auto key : keys[index] ){
-                if ( status[key] == 0 ){
-                    status[key]= 1 ;
-                    if ( hasBox[key] && !visited[key] ){
-                        q.push(key) ;
-                    }               
+        int cnd=0;
+        while(!q.empty()){
+            int b=q.front();
+            q.pop();
+            cnd+=candies[b];
+            for(int k:keys[b]){
+                if(!AvailableKeys[k]){
+                    AvailableKeys[k]=true;
+                    if(AvailableBoxes[k] && !visited[k]){
+                        q.push(k);
+                        visited[k]=true;
+                    }
                 }
             }
-            for ( auto it : containedBoxes[index] ){
-                hasBox[it] = true ;
-                if ( !visited[it] && status[it] == 1 ){
-                    q.push(it) ;
+            for(int c: containedBoxes[b]){
+                AvailableBoxes[c]=true;
+                if((AvailableKeys[c] || status[c]==1) && !visited[c]){
+                    q.push(c);
+                    visited[c]=true;
                 }
             }
         }
-        
-        return MAX ;
-
+        return cnd;
     }
 };
