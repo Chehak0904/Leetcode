af5120dabb215fc2be58ce099c2b877a65afd39e
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-06-14 23:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (17.12%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,46 +1,34 @@
 class Solution {
-public:
-    // int solve(int i,int j ,vector<vector<int>>&matrix,vector<vector<int>>&dp){
-    //     int n=matrix.size();
-    //     if(i==n-1) return matrix[i][j];
-    //     if(dp[i][j]!=INT_MAX)return dp[i][j];
-    //     int left=INT_MAX;
-    //     int right=INT_MAX;
-    //     int down=solve(i+1,j,matrix,dp);
-    //     if(j-1>=0){
-    //         left=solve(i+1,j-1,matrix,dp);
-    //     }
-    //     if(j+1<n){
-    //         right=solve(i+1,j+1,matrix,dp);
-    //     }
-    //     return dp[i][j]=min(down,min(left,right))+matrix[i][j];
+public: 
+    // int solve(int i,int j, vector<vector<int>>& dp, vector<vector<int>>& matrix){
+
+    //     if(i<0 || j>=matrix.size()) return 1e8;
+    //     if(i==0) return matrix[0][j];
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int leftdg=matrix[i][j]+solve(i-1,j-1,dp,matrix);
+    //     int up=matrix[i][j]+solve(i-1,j,dp,matrix);
+    //     int rightdg=matrix[i][j]+solve(i-1,j+1,dp,matrix);
+    //     return dp[i][j]=min({leftdg,up,rightdg});
     // }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int ans=INT_MAX;
         int n=matrix.size();
-        vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
-        for(int i=0;i<n;i++){
-            dp[n-1][i]=matrix[n-1][i];
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=matrix[n-1][j];
         }
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<n;j++){
-                int left=INT_MAX;
-                int right=INT_MAX;
-                if(j-1>=0){
-                    left=dp[i+1][j-1];
-                }
-                if(j+1<n){
-                    right=dp[i+1][j+1];
-                }
-                int down=dp[i+1][j];
-                dp[i][j]=min(left,min(right,down))+matrix[i][j];
+                int down=matrix[i][j]+dp[i+1][j];
+                int leftdg=1e8, rightdg=1e8;
+               if(j>0)  leftdg=matrix[i][j]+dp[i+1][j-1];
+               if(j<n-1)  rightdg=matrix[i][j]+dp[i+1][j+1];
+               dp[i][j]=min({down,leftdg,rightdg});
             }
         }
-        for(int i=0;i<n;i++){
-            ans=min(ans,dp[0][i]);
+        int ans=1e8;
+        for(int j=0;j<n;j++){
+            ans=min(ans,dp[0][j]);
         }
         return ans;
-
-        
     }
 };
