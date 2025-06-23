2d145b0064deade2588c50a181bc3f1c4c444306
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-06-23 19:34:40 +0530

    Time: 31 ms (21.35%) | Memory: 35.7 MB (10.88%) - LeetSync

--- 436-find-right-interval/find-right-interval.cpp
+++ 436-find-right-interval/find-right-interval.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    int find_interval(vector<vector<int>>& temp, unordered_map<int, int>& mp, int target) {
-        int i = 0;
-        int j = temp.size() - 1;
-        int ans = -1;
-        while (i <= j) {
-            int mid = (i + j) / 2;
-            if (temp[mid][0] >= target) {
-                ans = mid;
-                j = mid - 1;
-            } else {
-                i = mid + 1;
+    int find_right_interval(vector<vector<int>>&temp,int target,unordered_map<int,int>&mp){
+        int ans=-1;
+        int i=0;
+        int j=temp.size()-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(temp[mid][0]>=target){
+                j=mid-1;
+                ans=mid;
+            }
+            else{
+                i=mid+1;
             }
         }
-        if (ans == -1) return -1;
+        if(ans==-1){
+            return -1;
+        }
         return mp[temp[ans][0]];
     }
-
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        vector<int> ans(n);
-
-        unordered_map<int, int> mp; 
-        vector<vector<int>> temp = intervals;
-
-        for (int i = 0; i < n; i++) {
-            mp[intervals[i][0]] = i;
+        int n=intervals.size();
+        vector<int>ans(n);
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[intervals[i][0]]=i;
         }
-
-        sort(temp.begin(), temp.end()); 
-
-        for (int i = 0; i < n; i++) {
-            int target = intervals[i][1];
-            ans[i] = find_interval(temp, mp, target);
+        vector<vector<int>>temp=intervals;
+        sort(temp.begin(),temp.end());
+        for(int i=0;i<n;i++){
+            int t=find_right_interval(temp,intervals[i][1],mp);
+            ans[i]=t;
+            
         }
-
         return ans;
+
+        
     }
 };
