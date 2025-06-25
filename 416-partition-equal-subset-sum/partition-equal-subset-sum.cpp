52ecdf919ee7161d7d3d635b1eac85a4426492a3
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-06-25 19:22:32 +0530

    Time: 113 ms (67.10%) | Memory: 110.7 MB (24.45%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-    // int t[201][20001];
-    // bool solve(vector<int>&nums,int i,int x){
-    //     if(x==0) return true;
+    // // int t[201][20001];
+    // bool solve(vector<int>& nums,int i,int x,vector<vector<int>>& t){
+    //     if(x==0)return true;
     //     if(i>=nums.size()) return false;
     //     if(t[i][x]!=-1){
     //         return t[i][x];
     //     }
     //     bool take=false;
     //     if(nums[i]<=x){
-    //         take=solve(nums,i+1,x-nums[i]);
+    //         take=solve(nums,i+1,x-nums[i],t);
     //     }
-    //     bool not_take=solve(nums,i+1,x);
-    //     return t[i][x]=take||not_take;
+    //     bool not_take=solve(nums,i+1,x,t);
+    //     return t[i][x]= take|| not_take;
+
     // }
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int s = accumulate(nums.begin(), nums.end(), 0);
-        if (s % 2 != 0) return false;
-        int x = s / 2;
-
-        vector<vector<bool>> dp(n, vector<bool>(x + 1, false));
+        int n=nums.size();
+        int s=accumulate(nums.begin(),nums.end(),0);
         
-        
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
+        // memset(t,-1,sizeof(t));
+        if(s%2!=0)return false;
+        int x=s/2;
+        vector<vector<int>>dp(n,vector<int>(x+1,0));
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
         }
-        if (nums[0] <= x) dp[0][nums[0]] = true;
-
-        for (int i = 1; i < n; i++) {
-            for (int target = 1; target <= x; target++) {
-                bool not_take = dp[i - 1][target];
-                bool take = false;
-                if (nums[i] <= target) {
-                    take = dp[i - 1][target - nums[i]];
+        if(nums[0]<=x)dp[0][nums[0]]=true;
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=x;target++){
+                bool not_take=dp[i-1][target];
+                bool take =false;
+                if(nums[i]<=target){
+                    take=dp[i-1][target-nums[i]];
                 }
-                dp[i][target] = take || not_take;
+                dp[i][target]=take|not_take;
             }
         }
-        return dp[n - 1][x];
-        
+        return dp[n-1][x];
     }
 };
