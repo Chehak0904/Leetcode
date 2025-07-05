3016d2342e9175b83b7b6c937b573b50ace6b92f
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-07-05 10:26:08 +0530

    Time: 31 ms (54.44%) | Memory: 44.2 MB (39.17%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int recurrsive_sol(int idx1,int idx2,string&s,string& t,vector<vector<int>>&dp){
-        if(idx2==t.size()) return 1;
-        if(idx1==s.size()) return 0;
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-        int not_pick=recurrsive_sol(idx1+1,idx2,s,t,dp);
-        int pick=0;
-        if(s[idx1]==t[idx2]) pick=recurrsive_sol(idx1+1,idx2+1,s,t,dp);
-        return dp[idx1][idx2]=pick+not_pick;
-    }
     int numDistinct(string s, string t) {
-        vector<vector<int>>dp(s.length(),vector<int>(t.length(),-1));
-        return recurrsive_sol(0,0,s,t,dp);
+        int n = s.length();
+        int m = t.length();
+        vector<vector<double>> dp(n + 1, vector<double>(m + 1, 0));
+
         
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
