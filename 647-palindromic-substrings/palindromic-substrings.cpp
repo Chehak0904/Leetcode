ff6b2c552ba60f50ea1ffb48e51a1610e3dfc25b
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-07-10 10:41:20 +0530

    Time: 20 ms (41.11%) | Memory: 11.2 MB (46.92%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    bool check(int i,int j,string &s,vector<vector<int>>&dp){
-        if(i>=j)return true;
-        if(s[i]!=s[j])return false;
-        if(dp[i][j]!=-1) return dp[i][j];
-         return dp[i][j]=check(i+1,j-1,s,dp);
-    }
     int countSubstrings(string s) {
-        int n=s.length();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(check(i,j,s,dp)){
-                    cnt++;
+        int n = s.length();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int count = 0;
+
+        // All substrings of length 1 are palindromes
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+            count++;
+        }
+
+        // Substrings of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = true;
+                count++;
+            }
+        }
+
+        // Substrings of length 3 or more
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    count++;
                 }
             }
         }
-        return cnt;
-        
+
+        return count;
     }
 };
