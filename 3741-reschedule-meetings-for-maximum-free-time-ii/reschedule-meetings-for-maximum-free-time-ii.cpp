79eed1643a2fd6e780b3585103d22f0ef436b91e
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-07-10 18:18:08 +0530

    Time: 5 ms (91.24%) | Memory: 147.1 MB (68.13%) - LeetSync

--- 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
         int n=startTime.size();
-        vector<int>gap(n+1);
-        gap[0]=startTime[0];
+        vector<int> gap(n+1);
         gap[0]=startTime[0]-0;
         for(int i=1;i<n;i++){
             gap[i]=startTime[i]-endTime[i-1];
         }
         gap[n]=eventTime-endTime[n-1];
-        vector<int>leftmax(n+1),rightmax(n+1);
+        vector<int> leftmax(n+1), rightmax(n+1);
         leftmax[0]=gap[0];
         for(int i=1;i<=n;i++){
             leftmax[i]=max(leftmax[i-1],gap[i]);
@@ -18,16 +17,15 @@ public:
         for(int i=n-1;i>=0;i--){
             rightmax[i]=max(rightmax[i+1],gap[i]);
         }
-        int ans=INT_MIN;
+        int ans=leftmax[n];
         for(int i=0;i<n;i++){
             int dur=endTime[i]-startTime[i];
-            int leftgap=0,rightgap=0;
+            int leftgap=0, rightgap=0;
             if(i-1>=0){
                 leftgap=leftmax[i-1];
             }
             if(i+2<=n){
                 rightgap=rightmax[i+2];
-
             }
             if(leftgap>=dur || rightgap>=dur){
                 ans=max(ans,gap[i]+gap[i+1]+dur);
@@ -37,7 +35,5 @@ public:
             }
         }
         return ans;
-
-       
     }
 };
