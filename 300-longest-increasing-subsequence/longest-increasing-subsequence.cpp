bfaff167d9df28ccf468520bd251c0f88e0a8625
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-07-12 07:52:57 +0530

    Time: 276 ms (36.59%) | Memory: 296.3 MB (34.15%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-    int f(int i, int p, vector<int>& nums, vector<vector<int>>& dp) {
-        if (i >= nums.size()) return 0;
-        if (dp[i][p + 1] != -1) return dp[i][p + 1];
+    // int f(int i, int p, vector<int>& nums, vector<vector<int>>& dp) {
+    //     if (i >= nums.size()) return 0;
+    //     if (dp[i][p + 1] != -1) return dp[i][p + 1];
 
-        int not_take = f(i + 1, p, nums, dp);
-        int take = 0;
+    //     int not_take = f(i + 1, p, nums, dp);
+    //     int take = 0;
 
         
-        if (p == -1 || nums[i] > nums[p]) {
-            take = 1 + f(i + 1, i, nums, dp);
-        }
+    //     if (p == -1 || nums[i] > nums[p]) {
+    //         take = 1 + f(i + 1, i, nums, dp);
+    //     }
 
-        return dp[i][p + 1] = max(take, not_take);
-    }
+    //     return dp[i][p + 1] = max(take, not_take);
+    // }
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return 1;
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return f(0, -1, nums, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
+        for(int i=n-1;i>=0;i--){
+            for(int p=i-1;p>=-1;p--){
+                int not_take=dp[i+1][p+1];
+                int take=0;
+                if(p==-1 || nums[i]>nums[p]){
+                    take=1+dp[i+1][i+1];
+                }
+                dp[i][p+1]=max(take,not_take);
+            }
+        }
+        return dp[0][0];
+
+        
     }
 };
