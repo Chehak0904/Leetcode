c0a0c7814d8c6d9ebfc905b0a06808e9d9ce52f0
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-07-13 11:46:09 +0530

    Time: 12 ms (62.21%) | Memory: 42 MB (36.46%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
-    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        int n=worker.size();
-        int m=difficulty.size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            int val=worker[i];
-            int idx=-1;
-            for(int j=0;j<m;j++){
-                if(difficulty[j]<=val){
-                    if(idx==-1 || profit[idx]<profit[j]){
-                        idx=j;
-                    }
-                }
-            }
-            if(idx!=-1){
-                ans+=profit[idx];
+    
+    int find_ans(vector<pair<int,int>>& diff, int target) {
+        int ans = -1;
+        int i = 0, j = diff.size() - 1;
+        while (i <= j) {
+            int m = (i + j) / 2;
+            if (diff[m].first <= target) {
+                ans = m;
+                i = m + 1;
+            } else {
+                j = m - 1;
             }
         }
-        return ans;    
+        return ans;
+    }
+
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        vector<pair<int,int>> vec;
+        for (int i = 0; i < difficulty.size(); i++) {
+            vec.push_back({difficulty[i], profit[i]});
+        }
+
+        
+        sort(vec.begin(), vec.end());
+
+        
+        vector<int> max_profit(difficulty.size());
+        max_profit[0] = vec[0].second;
+        for (int i = 1; i < vec.size(); i++) {
+            max_profit[i] = max(max_profit[i - 1], vec[i].second);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < worker.size(); i++) {
+            int idx = find_ans(vec, worker[i]);
+            if (idx != -1) {
+                ans += max_profit[idx];
+            }
+        }
+
+        return ans;
     }
 };
