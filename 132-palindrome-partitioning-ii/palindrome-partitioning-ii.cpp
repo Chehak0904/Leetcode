4da2629f8be7a5f2ea7a1adebcc9431e5409da77
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-07-15 18:42:12 +0530

    Time: 1429 ms (49.55%) | Memory: 63 MB (11.72%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    bool isPalindrome(int i, int j, string& s) {
+    bool isPalindrome(int i, int j, string& s,vector<vector<int>>&dp) {
+        if(dp[i][j]!=-1) return dp[i][j];
         while (i < j) {
             if (s[i] != s[j])
                 return false;
             i++;
             j--;
         }
-        return true;
+        return dp[i][j]=true;
     }
-    int f(int i, int j, string& s, vector<vector<int>>& dp) {
+    int f(int i, int j, string& s, vector<int>& dp1,vector<vector<int>>&dp2) {
         if(i==j) return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (isPalindrome(i, j, s))
+        if (dp1[i] != -1)
+            return dp1[i];
+        if (isPalindrome(i, j, s,dp2))
             return 0;
         int ans = INT_MAX;
         for (int k = i; k < j; k++) {
-            if (isPalindrome(i, k, s)) {
-                ans = min(ans, 1 + f(k + 1, j, s, dp));
+            if (isPalindrome(i, k, s,dp2)) {
+                ans = min(ans, 1 + f(k + 1, j, s, dp1,dp2));
             }
         }
-        return dp[i][j] = ans;
+        return dp1[i] = ans;
     }
     int minCut(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return f(0, n - 1, s, dp);
+        vector<int>dp1(n,-1);
+        vector<vector<int>> dp2(n, vector<int>(n, -1));
+        return f(0, n-1, s, dp1,dp2);
     }
 };
