60b86a13cc5286553041e4d9401087805fb4742d
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-07-15 20:09:16 +0530

    Time: 252 ms (25.70%) | Memory: 213.8 MB (40.22%) - LeetSync

--- 2854-decremental-string-concatenation/decremental-string-concatenation.cpp
+++ 2854-decremental-string-concatenation/decremental-string-concatenation.cpp
@@ -1,30 +1,20 @@
 class Solution {
-public:
-    int solve(char s, char e, int i, vector<string>& words, vector<vector<vector<int>>>& dp) {
-        if (i >= words.size()) return 0;
-
-        if (dp[s - 'a'][e - 'a'][i] != -1) return dp[s - 'a'][e - 'a'][i];
-
-        char f = words[i][0];
-        char l = words[i].back();
-        int len = words[i].length();
-
+public: 
+    int fun(char s,char e, int i,vector<string>& words,vector<vector<vector<int>>> &dp){
+        if(i==words.size()) return 0;
+        char f=words[i][0];
+        char l=words[i].back();
+        int len=words[i].length();
+        if(dp[s-'a'][e-'a'][i]!=-1) return dp[s-'a'][e-'a'][i];        
+        int a=(e==f)? fun(s,l,i+1,words,dp)+len-1: fun(s,l,i+1,words,dp)+len;
         
-        int a = solve(s, l, i + 1, words, dp) + len - (e == f ? 1 : 0);
-
-        
-        int b = solve(f, e, i + 1, words, dp) + len - (l == s ? 1 : 0);
-
-        return dp[s - 'a'][e - 'a'][i] = min(a, b);
+        int b=(l==s)?fun(f,e,i+1,words,dp)+len-1: fun(f,e,i+1,words,dp)+len;
+        return dp[s-'a'][e-'a'][i]= min(a,b);
     }
-
     int minimizeConcatenatedLength(vector<string>& words) {
-        int n = words.size();
-        string prev = words[0];
-
-        
-        vector<vector<vector<int>>> dp(26, vector<vector<int>>(26, vector<int>(n, -1)));
-
-        return prev.size() + solve(prev[0], prev.back(), 1, words, dp);
+        int n=words.size();
+        string prev=words[0];
+        vector<vector<vector<int>>> dp(26,vector<vector<int>>(26,vector<int>(n,-1)));
+        return prev.size()+ fun(prev[0],prev.back(),1, words,dp);
     }
 };
