3915b52aa8af210ce4ac39b3893e95fd83fdc4e4
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-07-28 21:31:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (50.72%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -9,22 +9,21 @@ public:
         
         temp.push_back(nums[i]);
         f(i + 1, temp, ans, nums);
-        temp.pop_back();
+        temp.pop_back(); 
 
         
-        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+        i++;
+        while (i < nums.size() && nums[i] == nums[i - 1]) {
             i++;
         }
-
-       
-        f(i + 1, temp, ans, nums);
+        f(i, temp, ans, nums);
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());  
+        vector<vector<int>> ans;
         vector<int> temp;
-        sort(nums.begin(), nums.end());
-        f(0, temp, res, nums);
-        return res;
+        f(0, temp, ans, nums);
+        return ans;
     }
 };
