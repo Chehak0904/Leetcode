cf27e3acac80b388ac9f0185bd3226360af3bd0c
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-08-08 10:23:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.32%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    void f(int idx, int target, vector<int>& candidates, vector<int> temp,vector<vector<int>>& ans) {
+    void f(int idx, int target, vector<int>& candidates, vector<int>& temp,
+           vector<vector<int>>& ans) {
         if (target == 0) {
             ans.push_back(temp);
             return;
         }
-        if (target < 0) return;
-        if (idx >= candidates.size()) return;
-        temp.push_back(candidates[idx]);
-        f(idx, target - candidates[idx], candidates, temp, ans);
-        temp.pop_back();
+        if (idx >= candidates.size())
+            return;
+        if (candidates[idx] <= target) {
+            temp.push_back(candidates[idx]);
+            f(idx, target - candidates[idx], candidates, temp, ans);
+            temp.pop_back();
+        }
+
         f(idx + 1, target, candidates, temp, ans);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>temp;
-        vector<vector<int>>ans;
-        f(0,target,candidates,temp,ans);
+        vector<int> temp;
+        vector<vector<int>> ans;
+        f(0, target, candidates, temp, ans);
         return ans;
     }
 };
