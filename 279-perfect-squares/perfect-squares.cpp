4ab86fb1e44c94b350339182705bb7e9ba4f7644
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-08-12 22:44:17 +0530

    Time: 77 ms (42.14%) | Memory: 13.8 MB (41.03%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
-    int solve(int idx,int target,vector<int>&nums,vector<vector<int>>&dp){
-        if(idx>=nums.size()){
-            if(target==0) return 0;
-            return INT_MAX;
+    int func(int n,vector<int>&dp){
+        if(n==0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int ans=INT_MAX;
+        for(int i=1;i*i<=n;i++){
+            ans=min(ans,1+func(n-i*i,dp));
         }
-        if(dp[idx][target]!=-1)return dp[idx][target];
-        int take=INT_MAX;
-        if(target>=nums[idx]){
-             int t=solve(idx,target-nums[idx],nums,dp);
-             if(t!=INT_MAX) take=1+t;
-             else take=INT_MAX;
-        }
-        int not_take=solve(idx+1,target,nums,dp);
-        return dp[idx][target]= min(take,not_take);
-        
-
+        return dp[n]=ans;
     }
     int numSquares(int n) {
-        vector<int>arr;
-        int i=1;
-        while(true){
-            if(i*i<=n){
-                arr.push_back(i*i);
-            }
-            else{
-                break;
-            }
-            i++;
-        }
-        vector<vector<int>>dp(arr.size(),vector<int>(n+1,-1));
-        return solve(0,n,arr,dp);
-
+        vector<int>dp(n+1,-1);
+        return func(n,dp);
+        
     }
 };
