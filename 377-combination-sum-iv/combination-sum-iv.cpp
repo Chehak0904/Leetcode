d152727ff638b9f4c7a1b615c504b01046cf6cb2
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-08-12 23:28:34 +0530

    Time: 4 ms (29.11%) | Memory: 9.2 MB (68.37%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int MOD = 1e9+7;
-    int f(int idx,int target,vector<int>&nums,vector<vector<int>>&dp){
+    int func(vector<int>& nums, int target, vector<int>& dp) {
         if(target==0) return 1;
-        if(target<0) return 0;
-        if(idx>=nums.size()) return 0;
-        if(dp[idx][target]!=-1) return dp[idx][target];
-        int take=f(0,target-nums[idx],nums,dp);
-        int not_take=f(idx+1,target,nums,dp);
-        return dp[idx][target]=take+not_take;
+        if(dp[target]!=-1) return dp[target];
+        int ans = 0;
+        for (auto num : nums) {
+            if (num <= target) {
+                ans += func(nums,target-num,dp);
+            }
+        }
+        return dp[target] = ans;
     }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<vector<int>>dp(nums.size(),vector<int>(target+1,-1));
-        return f(0,target,nums,dp);
+        vector<int>dp(target+1,-1);
+        return func(nums,target,dp);
     }
 };
