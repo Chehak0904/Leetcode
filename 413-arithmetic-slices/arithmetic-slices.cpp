4fd9f41aacf2ba475b8080a3a9dd6aee83d5381c
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-08-19 18:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (24.92%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int n=nums.size();
-        if(n<=2) return 0;
-        int cnt=0;
-        int i=0;
-        int j=2;
-        int cd=nums[1]-nums[0];
-        while(j<n){
-            if(nums[j]-nums[j-1]==cd){
-                cnt+=j-2-i+1;
-                j++;
-            }
-            else{
-                i=j-1;
-                cd=nums[j]-nums[i];
-                j++;
-            }
+        if (nums.size() <= 2)
+            return 0;
+        int cd = nums[1] - nums[0];
+        int i = 0;
+        int j = 2;
+        int ans = 0;
+        while (j < nums.size()) {
+            int cd_ = nums[j] - nums[j - 1];
+            if (cd_ == cd) {
 
+                int len = j - i + 1;
+                if (len >= 3) {
+                    ans += len - 3 + 1;
+                }
+                j++;
+            } else {
+
+                i = j - 1;
+                cd = cd_;
+                j++;
+            }
         }
-        return cnt;
+        return ans;
     }
 };
