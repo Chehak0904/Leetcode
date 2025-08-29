2f988a02234bbd5c9c3f55fcf8961d1855c8e137
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-08-29 22:34:23 +0530

    Time: 202 ms (38.61%) | Memory: 13.8 MB (59.17%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int> &nums,vector<vector<int>>&dp) {
-        if (i > j)
-            return 0;
-         if(dp[i][j]!=-1) return dp[i][j];   
-        int ans = INT_MIN;
-        for (int k = i; k <= j; k++) {
-            int cost = solve(i, k - 1, nums,dp) + solve(k + 1, j, nums,dp) +
-                       nums[i - 1] * nums[k] * nums[j + 1];
-            ans = max(ans, cost);
+    int func(int i, int j,vector<int>& nums,vector<vector<int>> &dp){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans=-1e8;
+        for(int idx=i;idx<=j;idx++){
+            int cost=nums[i-1]*nums[idx]*nums[j+1]+func(i,idx-1,nums,dp)+func(idx+1,j,nums,dp);
+            ans=max(ans,cost);
         }
         return dp[i][j]=ans;
     }
     int maxCoins(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> temp(n + 2, 1);
-        for (int i = 0; i < n; i++) {
-            temp[i + 1] = nums[i];
-        }
-        int m=temp.size();
-        vector<vector<int>>dp(m,vector<int>(m,-1));
-        return solve(1,temp.size()-2,temp,dp );
+        int n=nums.size();
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return func(1,n,nums,dp);
     }
 };
