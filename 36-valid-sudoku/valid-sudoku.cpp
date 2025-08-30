a0b5319e7b02e89bb80f87b38639dc357a1695ac
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-08-30 10:45:53 +0530

    Time: 14 ms (7.58%) | Memory: 25.5 MB (27.19%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<char,unordered_set<int>>row;
-        unordered_map<char,unordered_set<int>>col;
-        unordered_map<char,unordered_set<int>>sqr;
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                char ele=board[i][j];
-                if(ele=='.') continue;
-                int r=i;
-                int c=j;
-                int s=(i/3)*3+(j/3);
-                if(row[r].find(ele)==row[r].end()) row[r].insert(ele);
-                else return false;
-                if(col[c].find(ele)==col[c].end()) col[c].insert(ele);
-                else return false;
-                if(sqr[s].find(ele)==sqr[s].end()) sqr[s].insert(ele);
-                else return false;
+        vector<unordered_set<char>> row(9), col(9),sqr(9);
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char ele = board[i][j];
+                if (ele == '.') continue;
+
+                int s = (i / 3) * 3 + (j / 3);  
+
+                if (row[i].count(ele) || col[j].count(ele) || sqr[s].count(ele))
+                    return false;
+
+                row[i].insert(ele);
+                col[j].insert(ele);
+                sqr[s].insert(ele);
             }
         }
         return true;
-
     }
 };
