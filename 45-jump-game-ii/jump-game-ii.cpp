fe483a0e4b444e2ecb50edd7ce05640e6a848647
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-08-31 10:24:14 +0530

    Time: 159 ms (14.94%) | Memory: 22.6 MB (8.81%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int func(int idx,vector<int>&nums,vector<int>&dp){
-        if(idx>=nums.size()-1) return 0;
+    int func(int idx, vector<int>& nums,vector<int>&dp) {
+        if(idx>=nums.size()-1) return 0; 
         if(dp[idx]!=-1) return dp[idx];
-        int ans=1e9;
-        for(int i=1;i<=nums[idx];i++){
-            ans=min(ans,1+func(idx+i,nums,dp));
+        int steps = nums[idx];
+        int ans = 1e9;
+        for (int i = 1; i <= steps; i++) {
+            int t = 1 + func(idx+i, nums,dp);
+            ans = min(t, ans);
         }
-       return dp[idx]=ans;
+        return dp[idx]= ans;
     }
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
+        int n=nums.size();
+        vector<int>dp(n,-1);
         return func(0,nums,dp);
-        
-        
     }
 };
