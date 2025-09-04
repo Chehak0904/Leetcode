a1e2d188a3368951d665b3d15569c23482d1be13
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-09-05 00:51:29 +0530

    Time: 1121 ms (12.80%) | Memory: 34.6 MB (17.72%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,35 +1,25 @@
 class Solution {
-public:
+public: 
+    bool solve(int i,int j,string &s,vector<vector<int>>&dp){
+        if(i>=j) return true;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==s[j]){
+            return solve(i+1,j-1,s,dp);
+        }
+        return dp[i][j]=false;
+    }
     int countSubstrings(string s) {
-        int n = s.length();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int count = 0;
-
-        // All substrings of length 1 are palindromes
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-            count++;
-        }
-
-        // Substrings of length 2
-        for (int i = 0; i < n - 1; i++) {
-            if (s[i] == s[i + 1]) {
-                dp[i][i + 1] = true;
-                count++;
-            }
-        }
-
-        // Substrings of length 3 or more
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
-                if (s[i] == s[j] && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    count++;
+        int n=s.length();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(i,j,s,dp)){
+                    cnt++;
                 }
             }
         }
-
-        return count;
+        return cnt;
+        
     }
 };
