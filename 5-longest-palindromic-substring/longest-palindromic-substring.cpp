cbba4fee498fccdaf01be3d36e5fd7ec2f7a9b49
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-09-05 00:50:45 +0530

    Time: 347 ms (17.67%) | Memory: 291.7 MB (12.62%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
+    bool solve(int i,int j,string &s,vector<vector<int>>&dp){
+        if(i>=j) return true;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]!=s[j]) return false;
+        return dp[i][j]= solve(i+1,j-1,s,dp);
+    }
     string longestPalindrome(string s) {
-        int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        int st = -1;
-        int maxLen = -1;
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = 1;
-            int len = 1;
-            if (len > maxLen) {
-                st = i;
-                maxLen = 1;
-            }
-        }
-        for (int i = 0; i < n - 1; i++) {
-            if (s[i] == s[i + 1]) {
-                dp[i][i + 1] = 1;
-                int len = 2;
-                if (len > maxLen) {
-                    st = i;
-                    maxLen = 2;
-                }
-            }
-        }
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
-                if (s[i] == s[j] && dp[i + 1][j - 1] == 1) {
-                    dp[i][j] = 1;
-                    int l = len;
-                    if (l > maxLen) {
-                        st = i;
-                        maxLen = l;
+        int n=s.length();
+        int idx=-1;
+        int maxLen=-1;
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(i,j,s,dp)){
+                    int len=j-i+1;
+                    if(len>maxLen){
+                        maxLen=len;
+                        idx=i;
                     }
                 }
             }
         }
-        return s.substr(st, maxLen);
+        return s.substr(idx,maxLen);
+
     }
 };
