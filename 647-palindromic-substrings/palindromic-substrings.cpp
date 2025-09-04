94565e5c9a39b934e2cfd26c4f071ceb039f897c
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-09-05 00:52:09 +0530

    Time: 42 ms (27.35%) | Memory: 34.7 MB (17.72%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,25 +1,26 @@
 class Solution {
-public: 
-    bool solve(int i,int j,string &s,vector<vector<int>>&dp){
-        if(i>=j) return true;
+public:
+    bool solve(string &s, int i, int j,vector<vector<int>> &dp){
+        if(i>=j){
+            return true;
+        }
         if(dp[i][j]!=-1) return dp[i][j];
         if(s[i]==s[j]){
-            return solve(i+1,j-1,s,dp);
+            return dp[i][j]= solve(s,i+1,j-1,dp);
         }
-        return dp[i][j]=false;
+        return dp[i][j]= false;
     }
     int countSubstrings(string s) {
         int n=s.length();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(solve(i,j,s,dp)){
+                if(solve(s,i,j,dp)){
                     cnt++;
                 }
             }
         }
         return cnt;
-        
     }
 };
