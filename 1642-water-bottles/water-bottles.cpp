074f921678ab226f92d5c1849ae0f903c9950e6b
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-10-01 22:28:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (45.09%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int full=numBottles;
-        int empty=0;
-        int ans=0;
-        while(!(full==0 && empty/numExchange==0)){
-            ans+=full;
-            int curr_empty=full;
-            int total_empty=curr_empty+empty;
-            full=total_empty/numExchange;
-            empty=total_empty%numExchange;
+        int filled = numBottles;
+        int empty = 0;
+        int ans = 0;
+        while (filled >= 1) {
+            ans += filled;
+            empty += filled;
+            filled = empty / numExchange;
+            empty = empty % numExchange;
         }
         return ans;
-        
     }
 };
