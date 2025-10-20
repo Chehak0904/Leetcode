d50a77663ed0731ed8f79e55a552e4f60f7d1044
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-10-20 16:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (42.92%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -3,13 +3,13 @@ public:
     int finalValueAfterOperations(vector<string>& operations) {
         int x=0;
         for(int i=0;i<operations.size();i++){
-            if(operations[i][0]=='-'|| operations[i][2]=='-'){
+            if(operations[i]=="--X" || operations[i]=="X--"){
                 x--;
             }
-            else x++;
+            else{
+                x++;
+            }
         }
         return x;
-
-        
     }
 };
