b90368bed2a085a2a33915e777e551c4865699f3
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-10-23 13:34:28 +0530

    Time: 4 ms (20.15%) | Memory: 15.1 MB (39.28%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,34 +1,23 @@
 class Solution {
-public: 
-    // int solve(int i,int j, vector<vector<int>>& dp, vector<vector<int>>& matrix){
-
-    //     if(i<0 || j>=matrix.size()) return 1e8;
-    //     if(i==0) return matrix[0][j];
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     int leftdg=matrix[i][j]+solve(i-1,j-1,dp,matrix);
-    //     int up=matrix[i][j]+solve(i-1,j,dp,matrix);
-    //     int rightdg=matrix[i][j]+solve(i-1,j+1,dp,matrix);
-    //     return dp[i][j]=min({leftdg,up,rightdg});
-    // }
+public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int j=0;j<n;j++){
-            dp[n-1][j]=matrix[n-1][j];
-        }
-        for(int i=n-2;i>=0;i--){
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++) dp[0][j]=matrix[0][j];
+        for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
-                int down=matrix[i][j]+dp[i+1][j];
-                int leftdg=1e8, rightdg=1e8;
-               if(j>0)  leftdg=matrix[i][j]+dp[i+1][j-1];
-               if(j<n-1)  rightdg=matrix[i][j]+dp[i+1][j+1];
-               dp[i][j]=min({down,leftdg,rightdg});
+                int a=1e9,b=1e9;
+                if(j-1>=0) a=dp[i-1][j-1];
+                if(j+1<n)  b=dp[i-1][j+1];
+                int c=dp[i-1][j];
+                dp[i][j]=min({a,b,c})+matrix[i][j];
             }
         }
-        int ans=1e8;
+        int ans=INT_MAX;
         for(int j=0;j<n;j++){
-            ans=min(ans,dp[0][j]);
+            ans=min(ans,dp[n-1][j]);
         }
         return ans;
+        
     }
 };
