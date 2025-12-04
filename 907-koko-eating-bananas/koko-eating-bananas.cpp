b4cb91cd8b5be56052dc91592937be6aeb0e6ffa
Chehak Makkar <144509321+Chehak0904@users.noreply.github.com>
2025-12-05 02:34:18 +0530

    Time: 4 ms (88.73%) | Memory: 22.8 MB (97.50%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& piles, int k, int h) {
-        int hr = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            hr += piles[i] / k;
-            if(hr>h) return false;
-             if (piles[i] % k != 0) { hr++; }
+    bool isPossible(int m,vector<int>&piles,int h){
+        long long cnt=0;
+        for(int i=0;i<piles.size();i++){
+              cnt+=piles[i]/m;
+              if(piles[i]%m!=0){
+                cnt++;
+              }
+
         }
-        return hr <= h;
+        return cnt<=h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
-        int l = 1;
-        int r = *max_element(piles.begin(),piles.end());
-        int ans = -1;
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (isPossible(piles,m,h)) {
-                ans = m;
-                r = m - 1;
-
-            } else {
-                l = m + 1;
+        int mini=1;
+        int maxi=*max_element(piles.begin(),piles.end());
+        int ans=-1;
+        while(mini<=maxi){
+            int m=(mini+maxi)/2;
+            if(isPossible(m,piles,h)){
+                ans=m;
+                maxi=m-1;
+            }
+            else{
+                mini=m+1;
             }
         }
         return ans;
+        
     }
 };
